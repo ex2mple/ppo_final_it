d99133f49e4a55bb770066fab2ee01998b2f52b2
ppo <ex2mple@yandex.ru>
2024-03-31 13:37:00 +0300

    add rooms_count

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def index():
     windows_for_flat = new_req["message"]['windows_for_flat']['data']
     windows_numbers = []
     floors_count = len(new_req["message"]['windows']['data'].keys())
+    rooms_count = new_req['message']['rooms_count']['data']
 
     k = 1
+    counter = 0
     for i in range(floors_count):
         floor = []
         o = 0
@@ -27,13 +29,15 @@ def index():
             k += 1
         for l in range(len(floor)):
             state = new_req["message"]['windows']['data'][f'floor_{i + 1}']
+            flag = 0
             for p in range(len(floor[l])):
                 floor[l][p] = (floor[l][p], state[o])
                 o += 1
         windows_numbers.append(floor)
 
 
-    return render_template('index.html', request=new_req, windows=windows_numbers)
+    return render_template('index.html', request=new_req, windows=windows_numbers, windows_for_room=" ".join(windows_for_flat),
+                           rooms_count=rooms_count)
 
 
 if __name__ == '__main__':
